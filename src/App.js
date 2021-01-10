0b625248bb0812357b436aebd49933cef24b1ec0
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2021-01-10 13:10:31 +0100

    setup app.js and basic css implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
+      <main>
+        Hello Weather App!
+      </main>
     </div>
   );
 }
